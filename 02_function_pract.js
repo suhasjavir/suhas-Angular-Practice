dbd22197a03830f82fc5fdc66b6be13a871ca440
suhasjavir <suhas.javir9850@gmail.com>
2022-12-20 21:35:54 +0530

    assignment commit
    assignment commit

---
+++ 02_function_pract.js
@@ -0,0 +1,8 @@
+function multilply(num1, num2){
+    var result = num1 * num2;
+        return result;
+}
+var mutliResult = multilply(56, 45);
+console.log(`the multiplication is: ${mutliResult}`);
+var addResult = mutliResult + 100;
+console.log(`After adding 100 in result: ${addResult}`);
